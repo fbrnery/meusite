bfced9a8cb39eb7f4dce902e1c3440fe7945f43c
Fabricio <fbrnery@gmail.com>
2024-01-13 18:28:51 -0300

    Gerei o home/index e configurei o bootstrap

---
+++ app/assets/javascript/applicattion.js
@@ -0,0 +1,12 @@
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require select2
+//= require_tree
+
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "bootstrap"; //vai importar o bootstrap inteiro
