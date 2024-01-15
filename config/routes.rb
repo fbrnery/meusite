bed1e88185c7e72d204625de49b532af60a77385
Fabricio <fbrnery@gmail.com>
2024-01-15 18:18:19 -0300

    Arrumei o header do site

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get 'home/skills'
+  get 'home/portfolio'
+  get 'home/contatos'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
