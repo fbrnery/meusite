215bd3d26abf4e080ed09f6feb460e24a3f665fa
Fabricio <fbrnery@gmail.com>
2024-01-16 18:53:25 -0300

    Página sobre nós

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  get 'home/skills'
+  get 'home/sobrenos'
   get 'home/portfolio'
   get 'home/contatos'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
